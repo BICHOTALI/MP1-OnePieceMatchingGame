7d48e6c76b1eaeb573f8d8688f01b35ebb0b6b68
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-06-12 21:29:15 -0400

    added basic CSS to cnavas and body and added hero img. Also found colors for characters.

---
+++ Code/game.js
@@ -0,0 +1,11 @@
+// game.js
+
+// Get the canvas element from the HTML document
+const canvas = document.getElementById('game-canvas');
+
+// Get the 2D rendering context for the canvas
+const ctx = canvas.getContext('2d');
+
+// Example: Draw a rectangle on the canvas
+ctx.fillStyle = 'red';
+ctx.fillRect(100, 100, 50, 50);
