867961fabcb1f0d043d5108276d3445fb54f4d89
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-06-17 10:22:56 -0400

    Variable cards created. Card elements stord within w querySelector. Created flipCard functon and event.listener.

--- Code/game.js
+++ Code/game.js
@@ -1,1 +1,11 @@
-const cards = document.querySelectorAll("memory-card")
+//Selects all elements that represent the memory cards in the game and stores them in the cards variable.
+const cards = document.querySelectorAll(".memory-card")
+
+//function that adds flip to the class of the memory card div
+function flipCard(){
+  this.classList.toggle('flip')
+}
+
+//LOOP THAT ADDS EVENT.LISTENER TO EACH CARD
+cards.forEach(card => card.addEventListener('click', flipCard))
+
